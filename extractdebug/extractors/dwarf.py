166492af9b5a7bb3744d08c0d5d316444597d3b1
Adam Zambrzycki <adam@zambrzycki.net>
2020-12-29 17:52:06 +0100

    Make extractor functions private

--- extractdebug/extractors/dwarf.py
+++ extractdebug/extractors/dwarf.py
@@ -64,9 +64,9 @@ class DwarfExtractor(Extractor):
         for cu in self.dwarf_info.iter_CUs():
             top_die = cu.get_top_DIE()
             if 'DW_AT_stmt_list' in top_die.attributes:
-                self.cu_files[cu.cu_offset] = self.parse_files_info(self.dwarf_info, cu.structs, top_die.attributes['DW_AT_stmt_list'].value)
+                self.cu_files[cu.cu_offset] = self.__parse_files_info(self.dwarf_info, cu.structs, top_die.attributes['DW_AT_stmt_list'].value)
 
-            elements += self._parse_compilation_unit(cu)
+            elements += self.__parse_compilation_unit(cu)
             cus.append(cu)
 
         base_dir = cus[0].get_top_DIE().attributes[Attribute.COMP_DIR].value
@@ -76,50 +76,50 @@ class DwarfExtractor(Extractor):
 
         return ExtractorResult(file, self.cu_files, elements, base_dir)
 
-    def _parse_children(self, die):
+    def __parse_children(self, die):
         elements = []
 
         for child in die.iter_children():
             if child.tag == Tag.CLASS_TYPE:
-                elements.append(self._parse_class_type(child))
+                elements.append(self.__parse_class_type(child))
             elif child.tag == Tag.UNION_TYPE:
-                elements.append(self._parse_union_type(child))
+                elements.append(self.__parse_union_type(child))
             elif child.tag == Tag.STRUCTURE_TYPE:
                 if Attribute.NAME not in child.attributes:
                     continue
 
-                elements.append(self._parse_struct_type(child))
+                elements.append(self.__parse_struct_type(child))
             elif child.tag == Tag.SUB_PROGRAM:
-                self._parse_sub_program(child)
+                self.__parse_sub_program(child)
             elif child.tag == Tag.NAMESPACE:
-                elements.append(self._parse_namespace(child))
+                elements.append(self.__parse_namespace(child))
             elif child.tag == Tag.TYPEDEF:
-                elements.append(self._parse_typedef(child))
+                elements.append(self.__parse_typedef(child))
 
         return elements
 
-    def _parse_compilation_unit(self, unit):
+    def __parse_compilation_unit(self, unit):
         top_die = unit.get_top_DIE()
-        return self._parse_children(top_die)
+        return self.__parse_children(top_die)
 
-    def _parse_namespace(self, die):
-        elements = self._parse_children(die)
+    def __parse_namespace(self, die):
+        elements = self.__parse_children(die)
         namespace = Namespace(
             name=die.attributes[Attribute.NAME].value if Attribute.NAME in die.attributes else None,
             elements=elements,
-            decl_file=self.get_file(die)
+            decl_file=self.__get_file(die)
         )
 
         return namespace
 
-    def _parse_typedef(self, die):
+    def __parse_typedef(self, die):
         return TypeDef(
             name=die.attributes[Attribute.NAME].value,
-            type=self._resolve_type(die),
-            decl_file=self.get_file(die)
+            type=self.__resolve_type(die),
+            decl_file=self.__get_file(die)
         )
 
-    def _parse_class_type(self, die):
+    def __parse_class_type(self, die):
         class_name = die.attributes[Attribute.NAME].value
         members = []
         inheritance_class = None
@@ -127,50 +127,50 @@ class DwarfExtractor(Extractor):
 
         for child in die.iter_children():
             if child.tag == Tag.INHERITANCE:
-                inheritance_accessibility = self._get_accessibility(child)
-                inheritance_class = self._resolve_type(child)
+                inheritance_accessibility = self.__get_accessibility(child)
+                inheritance_class = self.__resolve_type(child)
                 continue
 
-            members.append(self._parse_member(child))
+            members.append(self.__parse_member(child))
 
         return Class(
             name=class_name,
             members=members,
             inheritance_class=inheritance_class,
             inheritance_accessibility=inheritance_accessibility,
-            decl_file=self.get_file(die)
+            decl_file=self.__get_file(die)
         )
 
-    def _parse_struct_type(self, die):
+    def __parse_struct_type(self, die):
         class_name = die.attributes[Attribute.NAME].value
         members = []
 
         for child in die.iter_children():
-            members.append(self._parse_member(child))
+            members.append(self.__parse_member(child))
 
         return Struct(
             name=class_name,
             members=members,
-            decl_file=self.get_file(die)
+            decl_file=self.__get_file(die)
         )
 
-    def _parse_union_type(self, die):
+    def __parse_union_type(self, die):
         members = []
 
         for child in die.iter_children():
-            members.append(self._parse_member(child))
+            members.append(self.__parse_member(child))
 
         return Union(
             name=die.attributes[Attribute.NAME].value if Attribute.NAME in die.attributes else None,
             fields=members,
-            accessibility=self._get_accessibility(die),
-            decl_file=self.get_file(die)
+            accessibility=self.__get_accessibility(die),
+            decl_file=self.__get_file(die)
         )
 
-    def _parse_member(self, child):
+    def __parse_member(self, child):
         attrs = child.attributes
-        accessibility = self._get_accessibility(child)
-        class_type = self._resolve_type(child)
+        accessibility = self.__get_accessibility(child)
+        class_type = self.__resolve_type(child)
 
         # Tag specific attributes
         if child.tag == Tag.SUB_PROGRAM:
@@ -180,13 +180,13 @@ class DwarfExtractor(Extractor):
                 accessibility=accessibility,
                 static=Attribute.OBJECT_POINTER not in attrs,
                 offset=child.offset,
-                decl_file=self.get_file(child),
+                decl_file=self.__get_file(child),
                 fully_defined=False
             )
 
             for sub_child in child.iter_children():
                 if sub_child.tag == Tag.PARAMETER:
-                    param_type = self._resolve_type(sub_child)
+                    param_type = self.__resolve_type(sub_child)
                     method.direct_parameters.append(Parameter(  # TODO
                         name=sub_child.attributes[Attribute.NAME].value if Attribute.NAME in sub_child.attributes else None,
                         type=param_type,
@@ -199,7 +199,7 @@ class DwarfExtractor(Extractor):
             if class_type:
                 return Field(
                     name=attrs[Attribute.NAME].value if Attribute.NAME in attrs else b'ERROR_UNKNOWN',
-                    decl_file=self.get_file(child),
+                    decl_file=self.__get_file(child),
                     type=class_type,
                     accessibility=accessibility,
                     static=Attribute.EXTERNAL in attrs,
@@ -208,16 +208,16 @@ class DwarfExtractor(Extractor):
 
             type_die = child.get_DIE_from_attribute(Attribute.TYPE)
             if type_die.tag == Tag.UNION_TYPE:
-                return self._parse_union(child)
+                return self.__parse_union(child)
 
         return None
 
-    def _parse_union(self, die):
+    def __parse_union(self, die):
         type_die = die.get_DIE_from_attribute(Attribute.TYPE)
         members = []
 
         for child in type_die.iter_children():
-            member = self._parse_member(child)
+            member = self.__parse_member(child)
             if not member:
                 continue
 
@@ -226,21 +226,21 @@ class DwarfExtractor(Extractor):
 
         return Union(
             fields=members,
-            accessibility=self._get_accessibility(die)
+            accessibility=self.__get_accessibility(die)
         )
 
-    def _parse_sub_program(self, die):
+    def __parse_sub_program(self, die):
         if Attribute.SPECIFICATION not in die.attributes:
             return
 
         specification_die_offset = die.get_DIE_from_attribute(Attribute.SPECIFICATION).offset
         if specification_die_offset not in self._subprograms:
-            self._parse_member(die.get_DIE_from_attribute(Attribute.SPECIFICATION))
+            self.__parse_member(die.get_DIE_from_attribute(Attribute.SPECIFICATION))
 
         existing_method = self._subprograms[specification_die_offset]
         for child in die.iter_children():
             if child.tag == Tag.PARAMETER:
-                param_type = self._resolve_type(child)
+                param_type = self.__resolve_type(child)
                 # if Attribute.NAME in child.attributes:  # Make sure
                 existing_method.parameters.append(Parameter(
                     name=child.attributes[Attribute.NAME].value if Attribute.NAME in child.attributes else b'arg',
@@ -252,7 +252,7 @@ class DwarfExtractor(Extractor):
 
         existing_method.fully_defined = True
 
-    def parse_files_info(self, dwarf_info, structs, offset=0):
+    def __parse_files_info(self, dwarf_info, structs, offset=0):
         files = {}
 
         lineprog_header = struct_parse(structs.Dwarf_lineprog_header, dwarf_info.debug_line_sec.stream, offset)
@@ -261,7 +261,7 @@ class DwarfExtractor(Extractor):
 
         return files
 
-    def _resolve_type(self, die):
+    def __resolve_type(self, die):
         type = Type()
 
         if Attribute.TYPE not in die.attributes:
@@ -293,7 +293,7 @@ class DwarfExtractor(Extractor):
             type.name = entry.attributes[Attribute.NAME].value
 
             if Attribute.DECL_FILE in entry.attributes:
-                type.decl_file = self.get_file(entry)
+                type.decl_file = self.__get_file(entry)
 
             entry = entry._parent
             while entry and entry.tag == Tag.NAMESPACE:
@@ -306,13 +306,13 @@ class DwarfExtractor(Extractor):
         return type
 
     @staticmethod
-    def _get_accessibility(die):
+    def __get_accessibility(die):
         if Attribute.ACCESSIBILITY in die.attributes:
             return die.attributes[Attribute.ACCESSIBILITY].value
 
         return Accessibility.private.value
 
-    def get_file(self, die):
+    def __get_file(self, die):
         if Attribute.DECL_FILE not in die.attributes:
             return None
 
